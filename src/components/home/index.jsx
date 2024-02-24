c93048a9dd7bf4880c1dbdb76a2b5958f71c14a2
Aditi Marathe <aditi_marathe@persistent.com>
2024-02-24 16:07:31 +0530

    landing page commits

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -13,13 +13,12 @@ function Index() {
   return (
     <Container fixed>
       <div className="home-page-container">
-
-        <Grid container spacing={3}>
-          <Grid item xs>
+        <Grid container spacing={2}>
+          <Grid item xs={4}>
             <div>
-              <Card style={{ backgroundColor: "#001a23", color: "#D5D5D5" }}>
+              <Card style={{ backgroundColor: "#001a23", color: "#D5D5D5" ,borderRadius: 15, border: "1px solid #27272A"}}>
                 <CardContent>
-                  <Typography gutterBottom variant="h6" component="div">
+                  <Typography gutterBottom style={{fontSize :"17px"}} component="div">
                     Get Started
                   </Typography>
                   <Typography variant="body2" style={{ color: "#71717A" }}>
@@ -29,35 +28,95 @@ function Index() {
                 </CardContent>
                 <CardActions>
 
-                  <Button sx={{ marginLeft: "auto", color: '#2DD4BF' }} style={{textTransform: 'none'}} size="small">Explore Now</Button>
+                  <Button sx={{ marginLeft: "auto", color: '#2DD4BF' }} style={{ textTransform: 'none' }} size="small">Explore Now</Button>
                 </CardActions>
               </Card>
             </div>
           </Grid>
-          <Grid item xs>
-            <div>  <Card style={{ backgroundColor: "#001a23", color: "#D5D5D5" }}>
-              <CardContent>
-                <Typography gutterBottom variant="h6" component="div">
-                  User Guide
-                </Typography>
-                <Typography variant="body2" style={{ color: "#71717A" }}>
-                  Lizards are a widespread group of squamate reptiles, with over 6,000
-                  species, ranging across all continents except Antarctica
-                </Typography>
-              </CardContent>
-              <CardActions>
-
-                <Button sx={{ marginLeft: "auto", color: '#2DD4BF' }} style={{textTransform: 'none'}} size="small">Explore Now
-                </Button>
-              </CardActions>
-            </Card></div>
-          </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={4}>
             <div>
-              orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            <Card style={{ backgroundColor: "#001a23", color: "#D5D5D5" ,borderRadius: 15, border: "1px solid #27272A"}}>
+
+                <CardContent>
+                  <Typography gutterBottom style={{fontSize :"17px"}} component="div">
+                    Developer Guide
+                  </Typography>
+                  <Typography variant="body2" style={{ color: "#71717A" }}>
+                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                    species, ranging across all continents except Antarctica
+                  </Typography>
+                </CardContent>
+                <CardActions>
+
+                  <Button sx={{ marginLeft: "auto", color: '#2DD4BF' }} style={{ textTransform: 'none' }} size="small">Explore Now</Button>
+                </CardActions>
+              </Card>
+            </div>
+          </Grid>
+          <Grid item xs={4}>
+            <div>
+            <Card style={{ backgroundColor: "#001a23", color: "#D5D5D5" ,borderRadius: 15, border: "1px solid #27272A"}}>
+
+                <CardContent>
+                  <Typography gutterBottom style={{fontSize :"14px"}} component="div">
+                Recent Activity
+                  </Typography>
+                  <Typography style={{fontSize :"17px"}} style={{ color: "#71717A" }}>
+                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                    species, ranging across all continents except Antarctica
+                  </Typography>
+                </CardContent>
+                <CardActions>
+
+                  <Button sx={{ marginLeft: "auto", color: '#2DD4BF' }} style={{ textTransform: 'none' }} size="small">Explore Now</Button>
+                </CardActions>
+              </Card>
+            </div>
+          </Grid>
+          <Grid item xs={4}>
+            <div>
+              <Card style={{ backgroundColor: "#001a23", color: "#D5D5D5" ,borderRadius: 15, border: "1px solid #27272A"}}>
+
+                <CardContent>
+                  <Typography gutterBottom style={{fontSize :"17px"}} component="div">
+                  Use Cases
+                  </Typography>
+                  <Typography variant="body2" style={{ color: "#71717A" }}>
+                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                    species, ranging across all continents except Antarctica
+                  </Typography>
+                </CardContent>
+                <CardActions>
+
+                  <Button sx={{ marginLeft: "auto", color: '#2DD4BF' }} style={{ textTransform: 'none' }} size="small">Explore Now</Button>
+                </CardActions>
+              </Card>
+            </div>
+          </Grid>
+          <Grid item xs={4}>
+            <div>
+              <Card style={{ backgroundColor: "#001a23", color: "#D5D5D5" ,borderRadius: 15, border: "1px solid #27272A"}}>
+
+                <CardContent>
+                  <Typography gutterBottom style={{fontSize :"17px"}} component="div">
+                  APIs
+                  </Typography>
+                  <Typography variant="body2" style={{ color: "#71717A" }}>
+                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                    species, ranging across all continents except Antarctica
+                  </Typography>
+                </CardContent>
+                <CardActions>
+
+                  <Button sx={{ marginLeft: "auto", color: '#2DD4BF' }} style={{ textTransform: 'none' }} size="small">Explore Now</Button>
+                </CardActions>
+              </Card>
             </div>
           </Grid>
         </Grid>
+
+
+
       </div>
     </Container>
   )
