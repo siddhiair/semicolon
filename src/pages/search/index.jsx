97bd9ba6a658a9fc6e65fbfa3ed392d94384151e
kishorj840 <kishorj840@gmail.com>
2024-02-24 19:52:24 +0530

    search page configured

--- src/pages/search/index.jsx
+++ src/pages/search/index.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
 
 import AccordionDataRenderer from "../../components/AccordionDataRenderer";
 import Search from "../../components/search";
 import AppNavBar from "../../components/navbar";
-import Index  from "../../components/LandingPage";
+import Index from "../../components/LandingPage";
 
 import "./index.css";
 
-const SearchPage = () => {
+const SearchPage = ({ buttonsConfig }) => {
    const [appData, setAppData] = useState([
       {
          label: "Code",
@@ -22,8 +22,39 @@ const SearchPage = () => {
       },
    ]);
 
+   const [appDataToShow, setAppDataToShow] = useState([]);
+
+   const [filterButtonsConfig, setFiltersButtonConfig] =
+      useState(buttonsConfig);
+
    const [isDataFetchInProgress, setIsDataFetchInProgress] = useState(false);
    const [fetchErrorOccurred, setFetchErrorOccurred] = useState(false);
+
+   const handleFilterClick = (id) => {
+      const updatedButtonsConfig = filterButtonsConfig.map((buttonConfig) =>
+         buttonConfig.id !== id
+            ? buttonConfig
+            : { ...buttonConfig, selected: !buttonConfig.selected }
+      );
+      setFiltersButtonConfig(updatedButtonsConfig);
+   };
+
+   const updateAppData = () => {
+      const newData = [];
+      for (const config of filterButtonsConfig) {
+         for (const data of appData) {
+            if (config.selected && config.label === data.label) {
+               newData.push(data);
+            }
+         }
+      }
+      setAppDataToShow(newData);
+      debugger;
+   };
+
+   useEffect(() => {
+      updateAppData();
+   }, [filterButtonsConfig, appData]);
 
    const handleSearch = async (searchedValue) => {
       // Expecting following response from our backend, lets not process data on UI.
@@ -45,16 +76,35 @@ const SearchPage = () => {
       <>
          <AppNavBar />
          <div className="app-container">
-            <Search handleSearch={handleSearch} />
-         <Index/>
-            {/* <AccordionDataRenderer
-               tabsData={appData}
+            <Search
+               shouldShowHeader={false}
+               showFilterButtonHorizontal={true}
+               handleSearch={handleSearch}
+               buttonsConfig={filterButtonsConfig}
+               onFilterClick={handleFilterClick}
+            />
+            {/* <Index /> */}
+            <AccordionDataRenderer
+               tabsData={appDataToShow}
                isDataFetchInProgress={isDataFetchInProgress}
                fetchErrorOccurred={fetchErrorOccurred}
-            /> */}
+            />
          </div>
       </>
    );
+};
+
+SearchPage.defaultProps = {
+   buttonsConfig: [
+      { id: 1, label: "Code", name: "Code Search", selected: false },
+      { id: 2, label: "Document", name: "Jira Search", selected: false },
+      {
+         id: 3,
+         label: "Code & Document",
+         name: "Confluence Search",
+         selected: false,
+      },
+   ],
 };
 
 export default SearchPage;
