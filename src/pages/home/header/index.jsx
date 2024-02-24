31f27ddd7908a2f7af7c3007a559cef97e5282a4
kishorj840 <kishorj840@gmail.com>
2024-02-24 16:16:30 +0530

    css added for home page

--- src/pages/home/header/index.jsx
+++ src/pages/home/header/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
+import "./index.css";
 const Header = () => {
    return (
-      <div>
-         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum,
-         praesentium.
+      <div className="header">
+         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque
+         aspernatur facere, suscipit numquam quos neque nesciunt qui iusto
+         dolorem aliquam.
       </div>
    );
 };
