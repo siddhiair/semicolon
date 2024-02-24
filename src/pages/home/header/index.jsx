c702b7cef3f4dd19f03073f6e86e531ef6277cce
siddhiair <siddhiair8@gmail.com>
2024-02-25 02:01:59 +0530

    home updated

--- src/pages/home/header/index.jsx
+++ src/pages/home/header/index.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { Box } from "@mui/material";
+import { logo } from "../../../assets/images";
 
 import "./index.css";
 const Header = () => {
   return (
-    <div className="header">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque
-      aspernatur facere, suscipit numquam quos neque nesciunt qui iusto dolorem
-      aliquam.
-    </div>
+    <Box sx={{ p: "2em" }}>
+      <div>
+        <a href="/">
+          <img src={logo} width={140} height={36} alt="Logo" />
+        </a>
+      </div>
+      <h1 className="login-card-title">
+        At vero eos et accusamus et iusto odio dignissimos ducimus
+        <span>voluptatum delenit</span>
+      </h1>
+    </Box>
   );
 };
 
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,24 +3,18 @@ import { useNavigate } from "react-router-dom";
 
 import Header from "./header";
 import Footer from "./footer";
-import AppNavBar from "../../components/navbar";
-
 import "./index.css";
 
 const HomePage = () => {
-   const navigate = useNavigate();
-   return (
-      <>
-         <AppNavBar />
-         <div className="main-container">
-            <Header />
-            <div className="hero-image">
-               <img src="src/assets/images/hero.png" alt="" />
-            </div>
-            <Footer />
-         </div>
-      </>
-   );
+  return (
+    <div className="login-card">
+      <Header />
+      <div className="hero-image">
+        <img src="src/assets/images/hero.png" alt="" width={600} height={400} />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default HomePage;
